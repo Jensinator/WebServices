fece8ae6143cfb8ef6777f4715636f8a46d6520f
Jens Leuschner <s3jeleus@stud.hszg.de>
2015-01-07 10:16:55 +0100

    Add new class for service to receive

--- project/src/main/java/de/hszg/service/MeasurementReceiver.java
+++ project/src/main/java/de/hszg/service/MeasurementReceiver.java
@@ -1,5 +1,16 @@
 package de.hszg.service;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/receiver")
 public class MeasurementReceiver {
 
+//	
+//	@POST
+//	@Path("/sendViews")
+//	//@PathParam("/{pageViews}")
+//	@Consumes(MediaType.APPLICATION_XML)
+//	public Response sendViews( Sensor sensor ){
+//	}
 }
