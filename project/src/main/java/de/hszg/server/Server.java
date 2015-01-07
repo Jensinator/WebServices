5a71d8240dbc4a9ff881c8eb8c24c659bff0b2fc
s3rowoll <romano-wollstein@web.de>
2015-01-07 12:16:24 +0100

    added Server

---
+++ project/src/main/java/de/hszg/server/Server.java
@@ -0,0 +1,25 @@
+package de.hszg.server;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import de.hszg.client.Sensor;
+import de.hszg.service.SensorCollection;
+
+@Path("/server")
+public class Server {
+
+	@POST
+	@Path("/sendCollection")
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response sendCollection( SensorCollection sensorCollection){
+		
+		// do here the stuff we want to do with our service
+		
+		
+		return Response.status(200).entity( "Everything was good" ).build();
+	}
+}
