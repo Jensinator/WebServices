aa1d5614152b221a180386813bc37ef60ce1314f
s3rowoll <romano-wollstein@web.de>
2015-01-07 09:44:03 +0100

    Class Measurement implemented

--- project/src/main/java/de/hszg/client/Measurement.java
+++ project/src/main/java/de/hszg/client/Measurement.java
@@ -1,5 +1,47 @@
 package de.hszg.client;
 
-public class Measurement {
+import java.sql.Timestamp;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "measurement")
+public class Measurement {
+							
+		private Timestamp timestamp;
+		private long value;
+		private int id;
+		
+		public Measurement(){
+			
+		}
+		
+		@XmlElement(name = "timestamp")
+		public Timestamp getTimestamp() {
+			return timestamp;
+		}
+		
+		public void setTimestamp(Timestamp timestamp) {
+			this.timestamp = timestamp;
+		}
+		
+		@XmlElement(name = "value")
+		public long getValue() {
+			return value;
+		}
+		
+		public void setValue(long value) {
+			this.value = value;
+		}
+		
+		@XmlElement(name = "id")
+		public int getId() {
+			return id;
+		}
+		
+		public void setId(int id) {
+			this.id = id;
+		}
+		
+		
 }
