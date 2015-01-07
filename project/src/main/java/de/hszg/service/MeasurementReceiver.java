bcfc8de6b8120b9ff57b947ab2528649fb1df88c
Jens Leuschner <s3jeleus@stud.hszg.de>
2015-01-07 10:35:28 +0100

    Set up a deployable Service

--- project/src/main/java/de/hszg/service/MeasurementReceiver.java
+++ project/src/main/java/de/hszg/service/MeasurementReceiver.java
@@ -1,16 +1,38 @@
 package de.hszg.service;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import de.hszg.client.Sensor;
 
 @Path("/receiver")
 public class MeasurementReceiver {
 
-//	
-//	@POST
-//	@Path("/sendViews")
-//	//@PathParam("/{pageViews}")
-//	@Consumes(MediaType.APPLICATION_XML)
-//	public Response sendViews( Sensor sensor ){
-//	}
+	
+	@POST
+	@Path("/sendValues")
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response sendViews( Sensor sensor ){
+		
+		// do here the stuff we want to do with our service
+		
+		
+		return Response.status(200).entity( "Everything was good" ).build();
+	}
+	
+	@GET
+	@Path("/getHello")
+	@Produces(MediaType.APPLICATION_XML)
+	public String sayHtmlHello() {
+		
+	    return "<html> " + "<title>" + "Hello" + "</title>"
+	        + "<body><h1>" + "Hello" + "</body></h1>" + "</html> ";
+	}
+	
+	
 }
